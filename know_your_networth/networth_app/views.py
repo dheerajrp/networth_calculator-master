e1734c9d62a58a71d6dbed15197d0e96458d3304
dheerajrp <dheerajrp66@gmail.com>
2025-03-21 20:16:25 +0530

    added multiple instruments

--- know_your_networth/networth_app/views.py
+++ know_your_networth/networth_app/views.py
@@ -6,9 +6,12 @@ def home(request):
         form = NetWorthForm(request.POST)
         if form.is_valid():
             data = form.cleaned_data
-            net_worth = (data['liquid_money'] + data['stocks_value'] +
-                         data['mutual_funds'] + data['us_stocks'] +
-                         data['crypto'] + data['debt_funds'] - data['liabilities'])
+            net_worth = (
+                data['liquid_money'] + data['fds'] + data['stocks_value'] + data['mutual_funds'] + data['us_stocks'] +
+                data['crypto'] + data['debt_funds'] + data['epf'] + data['ppf'] + data['vpf'] + data['nps'] +
+                data['sukanya_samruddhi_yojna'] + data['senior_citizen_savings_scheme'] + data['jewellery'] + data['sgb'] + 
+                data['gold_etf'] + data['digital_gold'] + data['silver'] + data['home_value'] + data['land_value'] + data['reits']
+            )
             return render(request, 'networth_app/result.html', {'net_worth': net_worth})
     else:
         form = NetWorthForm()
